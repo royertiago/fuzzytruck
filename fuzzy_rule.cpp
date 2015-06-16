ccf2e5f99b1a5d26d65ae2e83fbfa658c365ee7e
Tiago Royer <royertiago@gmail.com>
2015-06-16 00:27:26 -0300

    Add class FuzzyRule.

---
+++ fuzzy_rule.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include "fuzzy_rule.h"
+
+FuzzyRule::FuzzyRule( FuzzySet i1, FuzzySet i2, FuzzySet i3, FuzzySet output ) :
+    input{ i1, i2, i3 },
+    output( output )
+{}
+
+double FuzzyRule::intensity( double x, double y, double z, double out ) {
+    return std::max( std::max( std::max(
+        input[0](x),
+        input[1](y)),
+        input[2](z)),
+        output(out)
+    );
+}
---
+++ fuzzy_rule.h
@@ -0,0 +1,27 @@
+#ifndef FUZZY_RULE_H
+#define FUZZY_RULE_H
+
+/* Class that encapsulates a single fuzzy rule.
+ *
+ * For simplicity, we will treat only the special case
+ * with three inputs combined with AND.
+ */
+
+#include "fuzzy_set.h"
+
+class FuzzyRule {
+    FuzzySet input[3];
+    FuzzySet output;
+
+public:
+    FuzzyRule( FuzzySet, FuzzySet, FuzzySet, FuzzySet output );
+
+    /* Returns the intensity of which this rule is actived
+     * for the given output value.
+     *
+     * Se the documentation for details.
+     */
+    double intensity( double, double, double, double output_value );
+};
+
+#endif // FUZZY_RULE_H
